aaf1a55970d76402da5788a5eacac46c90e185f3
arnavsharma9573 <arnav.sharma@thewasserstoff.com>
2025-10-25 14:34:51 +0530

    photo-opmti

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ComplianceHero from "@/components/landing/ComplianceHero";
 import HeroSection from "@/components/landing/HeroSection";
 import HowItWorks from "@/components/landing/HowItWorks";
@@ -8,7 +9,17 @@ import WhoStarLineIsFor from "@/components/landing/Working";
 export default function Home() {
   return (
     <>
-      <div className="absolute top-0 left-0 right-0 h-[75vh] bg-[url('/BG-image.svg')] bg-no-repeat bg-left-top bg-cover -z-10 opacity-98" />
+      <div className="absolute top-0 left-0 right-0 h-[75vh] -z-10 opacity-98">
+        <Image
+          src="/BG-image.svg"
+          alt="Background decoration"
+          fill
+          priority
+          quality={75}
+          className="object-cover object-left-top"
+          sizes="100vw"
+        />
+      </div>
       <div className="relative z-10">
         <div className="min-h-[70vh] md:min-h-[60vh]">
           <HeroSection />
